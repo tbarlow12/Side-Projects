9eec623e7a4333aaa281661a828107d2ebe54a29
tbarlow12 <tanner.barlow12@gmail.com>
2016-09-20 17:42:04 -0600

    Remove old Organizer project

--- Organizer/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Tanner on 9/14/2016.
- */
-public class Main {
-    public static void main(String[] args){
-        Organizer o = new Organizer();
-        System.out.println("Hello");
-    }
-}
